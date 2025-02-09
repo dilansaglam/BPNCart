87ebdead9eaccf817a95ee2bd5b20c1b39de1c73
Dilan Saglam <Dilan.Saglam@boyner.com.tr>
2025-02-09 20:28:15 +0300

    add exception handling and logging behavior

---
+++ BPNCart.Application/Behaviors/ExceptionHandlingAndLogging.cs
@@ -0,0 +1,33 @@
+﻿using BPNCart.Domain.Responses.Base;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace BPNCart.Application.Behaviors;
+public class ExceptionHandlingAndLogging<TRequest, TResponse>(ILogger<ExceptionHandlingAndLogging<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : BaseResponse, new()
+{
+    private readonly ILogger<ExceptionHandlingAndLogging<TRequest, TResponse>> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Executing {typeof(TRequest).Name}. Request: {JsonSerializer.Serialize(request)}");
+
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error executing {typeof(TRequest).Name}. Exception: {ex.InnerException?.Message ?? ex.Message}");
+
+            return new TResponse
+            {
+                Result = false,
+                Message = ex.InnerException?.Message ?? ex.Message
+            };
+
+        }
+    }
+}
---
+++ BPNCart.Application/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using BPNCart.Application.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BPNCart.Application;
+public static class ServiceRegistration
+{
+    public static void AddApplicationService(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(cfg => {
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ExceptionHandlingAndLogging<,>));
+            cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly);
+        });
+    }
+}
